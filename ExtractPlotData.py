2081ce8e3b565d6524d6419a4b8cbe24342487f1
SteveMaas1978 <steve.maas@utah.edu>
2025-02-02 13:24:58 -0700

    Added post examples
    Added couple of example python scripts that use the post module.

---
+++ ExtractPlotData.py
@@ -0,0 +1,23 @@
+# @fbs { 
+#  "name" : "Extract Plot Data",
+#  "info" : "This tool extracts data from a post model."
+# }
+from fbs import post
+
+postModel = post.GetActiveModel()
+
+dataField = postModel.GetDataField("stress")
+
+# I think this changes the internal state of the post-model.
+# Is this a problem?
+state = postModel.Evaluate(dataField, post.MAT3DS.P1, postModel.GetStates() - 1)
+
+faceData = state.faceData
+
+mesh = postModel.GetFEMesh(0)
+for index in range(mesh.Surfaces()):
+    surface = mesh.Surface(index)
+    print(surface.GetName())
+    faces = surface.GetFaceIndices()
+    for face in faces:
+        print(str(face) + ", " + str(faceData[face].val))
